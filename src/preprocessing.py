d03d09883ac3d5e36275a432af1d13de00e531eb
Sooryasooru <soorya0286@gmail.com>
2026-04-01 17:54:06 +0530

    final1 3rd try

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,6 +3,6 @@ from sklearn.preprocessing import StandardScaler
 def scale_data(X_train,X_test):
     scaler = StandardScaler()
     X_test_scaler = scaler.fit_transform(X_test)
-    X_train_scaler = scaler.fit_transfrom(X_train)
+    X_train_scaler = scaler.fit_transform(X_train)
     return X_test,X_train
 
